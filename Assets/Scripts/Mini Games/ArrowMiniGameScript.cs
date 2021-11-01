28b5d87baabcd543da1db6b3202fb274b1b50f37
Jigracia <jigracia@miuandes.cl>
2021-11-01 14:28:28 -0300

    Fixed minigames Hardcoded 1080p

--- Assets/Scripts/Mini Games/ArrowMiniGameScript.cs
+++ Assets/Scripts/Mini Games/ArrowMiniGameScript.cs
@@ -16,8 +16,8 @@ public class ArrowMiniGameScript : MonoBehaviour
     public Inventory inventory;
     public Text countDownText;
     private bool stopper=true;
-    private float currentTime= 100f;
-    public float pointerSpeed=0.5f;
+    private float currentTime=6f;
+    public float pointerSpeed=3000f;
     private float pointerPosition;
     private float maxRedZone;
     private float minRedZone;
@@ -48,70 +48,70 @@ public class ArrowMiniGameScript : MonoBehaviour
 
         //RED ZONE
         auxRedZone=(RectTransform)RedZone.transform;
-        maxRedZone=(auxRedZone.rect.width/2)+5;
-        minRedZone=(auxRedZone.rect.width/-2)+5;
+        maxRedZone=1650;
+        minRedZone=300;
         //Debug.Log("Red "+maxRedZone+" "+minRedZone);
 
         //YELLOW ZONE
         auxYellowZone=(RectTransform)YellowZone.transform;
-        maxYellowZone=(auxYellowZone.rect.width/2)+5;
-        minYellowZone=(auxYellowZone.rect.width/-2)+5;
+        maxYellowZone=1450;
+        minYellowZone=500;
         //Debug.Log("Yellow "+maxYellowZone+" "+minYellowZone);
 
         //BROWN ZONE
         auxBrownZone=(RectTransform)BrownZone.transform;
-        maxBrownZone=(auxBrownZone.rect.width/2)+5;
-        minBrownZone=(auxBrownZone.rect.width/-2)+5;
+        maxBrownZone=1230;
+        minBrownZone=715;
         //Debug.Log("Brown "+maxBrownZone+" "+minBrownZone);
 
         //GREEN ZONE
         auxGreenZone=(RectTransform)GreenZone.transform;
-        maxGreenZone=(auxGreenZone.rect.width/2)+5;
-        minGreenZone=(auxGreenZone.rect.width/-2)+5;
+        maxGreenZone=1050;
+        minGreenZone=900;
         //Debug.Log("Green "+maxGreenZone+" "+minGreenZone);
     }
 
     // Update is called once per frame
     public void ArrowUpdate()
     {
-        //Timer Logic
-        if(currentTime>0f && stopper){       
-                currentTime -= 1* Time.unscaledDeltaTime;
-                countDownText.text=currentTime.ToString("0");
-        }
-
-        //Key Logic
-        if (Input.GetKeyDown("e") && stopper) {
-            pointerSpeed=0;
-            stopper=false;
-            if(pointerPosition<minBrownZone && pointerPosition>maxBrownZone){
-                Success.SetActive(true);
-            }else{
-                Fail.SetActive(true);
+        if(gameObject.activeSelf && stopper){
+            //Timer Logic
+            if(currentTime>0f && stopper){       
+                    currentTime -= 1* Time.unscaledDeltaTime;
+                    countDownText.text=currentTime.ToString("0");
             }
-            //Damage Logic
-            StartCoroutine(waiterNReset(pointerPosition));
+
+            //Key Logic
+            if (Input.GetKeyDown("e") && stopper) {
+                pointerSpeed=0;
+                stopper=false;
+                if(Pointer.transform.position.x>minGreenZone && Pointer.transform.position.x<maxGreenZone){
+                    Success.SetActive(true);
+                }else{
+                    Fail.SetActive(true);
+                }
+                //Damage Logic
+                StartCoroutine(waiterNReset(Pointer.transform.position.x));
+            }
+
+            //TimeoutLogic
+            if(currentTime<0f && stopper){
+                pointerSpeed=0;
+                stopper=false;
+                Fail.SetActive(true);
+                //Player Recive full damage
+                StartCoroutine(waiterNReset(maxRedZone+10f));
+            }
+
+
+            //MOVIMIENTO FLECHA
+            if(Pointer.transform.position.x>maxRedZone || Pointer.transform.position.x<minRedZone){
+                pointerSpeed *= -1; 
+            }
+            Pointer.transform.position= new Vector3(Pointer.transform.position.x+(pointerSpeed*Time.unscaledDeltaTime),103,0);
+            Debug.Log("Pointer x loc "+(Pointer.transform.position.x));
+            //pointerPosition+=pointerSpeed;
         }
-
-        //TimeoutLogic
-        /*if(currentTime<0f && stopper){
-            pointerSpeed=0;
-            stopper=false;
-            Fail.SetActive(true);
-            //Player Recive full damage
-            StartCoroutine(waiterNReset(maxRedZone+10f));
-        }*/
-
-
-        //MOVIMIENTO FLECHA
-        /*if(pointerPosition>maxRedZone || pointerPosition<minRedZone){
-            pointerSpeed *= -1;
-            
-        }*/
-        Pointer.transform.position= new Vector3(Pointer.transform.position.x+(pointerSpeed*Time.unscaledDeltaTime),Pointer.transform.position.y,0);
-        Debug.Log("Pointer x loc "+Pointer.transform.position.x);
-        //pointerPosition+=pointerSpeed;
-        
     }
 
     void ResetVariables(float position){
@@ -119,8 +119,7 @@ public class ArrowMiniGameScript : MonoBehaviour
         Fail.SetActive(false);
         Success.SetActive(false);
         currentTime=6f;
-        pointerSpeed=1.5f;
-        stopper=true;
+        pointerSpeed=3000f;
         //gameObject.SetActive(false);
         gameObject.transform.localScale = new Vector3(0, 0, 0);
         //SceneManager.LoadScene(0);
@@ -128,27 +127,28 @@ public class ArrowMiniGameScript : MonoBehaviour
 
     void DamageCalc(float position){
         Debug.Log(position);
-        if(pointerPosition<minYellowZone || pointerPosition>maxYellowZone){
+        Debug.Log(minYellowZone+" "+maxYellowZone);
+        if(position>minGreenZone && position<maxGreenZone){
             //Red Zone
-            Debug.Log("Red Zone");
+            Debug.Log("Green Zone");
             //Fail.SetActive(true);
-            inventory.TakeDamage(50);
-        }else if(pointerPosition<minBrownZone || pointerPosition>maxBrownZone){
+            inventory.TakeDamage(10);
+        }else if(position>minBrownZone && position<maxBrownZone){
             //Yellow Zone
-            Debug.Log("Yellow Zone");
-            //Fail.SetActive(true);
-            inventory.TakeDamage(40);
-        }
-        else if(pointerPosition<minGreenZone || pointerPosition>maxGreenZone){
-            //Brown Zone
             Debug.Log("Brown Zone");
             //Fail.SetActive(true);
             inventory.TakeDamage(30);
+        }
+        else if(position>minYellowZone && position<maxYellowZone){
+            //Brown Zone
+            Debug.Log("Yellow Zone");
+            //Fail.SetActive(true);
+            inventory.TakeDamage(40);
         }else{
             //Green Zone
-            Debug.Log("Green Zone");
+            Debug.Log("Red Zone");
             //Success.SetActive(true);
-            inventory.TakeDamage(10);
+            inventory.TakeDamage(50);
         }
     }
 
@@ -164,6 +164,7 @@ public class ArrowMiniGameScript : MonoBehaviour
         ResetVariables(position);
         yield return new WaitForSecondsRealtime(1);
         Time.timeScale = 1;
+        stopper=true;
         gameObject.SetActive(false);
     }
 }
