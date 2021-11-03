c1ab0ae79f1af3e26a710e7015fc167c5f6f4189
Andres Vial <avial5@miuandes.cl>
2021-11-03 10:47:19 -0300

    Small fixes for zombie movement and sound

--- Assets/Scripts/Enemy/EnemyMovement.cs
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -6,13 +6,13 @@ public class EnemyMovement : MonoBehaviour
 {
     public void UpdateMovement(Enemy enemy, float velocity)
     {
-        if (enemy.goDown)
+        /*if (enemy.goDown)
         {
             enemy.transform.Translate(Vector2.down * velocity * Time.deltaTime);
         }
         else
         {
             enemy.transform.Translate(Vector2.up * velocity * Time.deltaTime);
-        }
+        }*/
     }
 }
